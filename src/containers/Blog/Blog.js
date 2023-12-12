6ce5d8cd6ea4d29c34d708a0571466fd2f5b63e1
ardalan shahandeh <ardalan.shahande@gmail.com>
2023-12-12 08:36:46 +0330

    fix: Create Blog And New Post

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,63 @@
+import React from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+
+import "./Blog.css";
+
+class Blog extends React.Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((item) => {
+        return {
+          ...item,
+          author: "Ardalan",
+        };
+      });
+
+      // this.setState({ posts: response.data });
+      this.setState({ posts: updatedPosts });
+    });
+  }
+
+  selectPostHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  render() {
+    const posts = this.state.posts.map((item) => {
+      return (
+        <Post
+          key={item.id}
+          title={item.title}
+          author={item.author}
+          click={() => this.selectPostHandler(item.id)}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <section className="posts">{posts}</section>
+
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
